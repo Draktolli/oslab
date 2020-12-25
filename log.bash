9350d01333481e46d96e0102c7929a12e55ef3f1
Draktolli <70760304+Draktolli@users.noreply.github.com>
2020-12-25 22:16:08 +0300

    Add files via upload

---
+++ log.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! [ -f "/var/log/anaconda/X.log" ]; then
+  echo "Error: log файла не существует" >&2; exit -7
+else
+cat /var/log/anaconda/X.log |grep "(WW)"|grep -v "warning"|sed "s/(WW)/\x1b[93mWarning:\x1b[0m/g"
+cat /var/log/anaconda/X.log |grep "(II)"|grep -v "informational"|sed "s/(II)/\x1b[36mInformation:\x1b[0m/g"
+fi
