5d5b3a8e1cd163d97274e921c93ebab589330388
Draktolli <70760304+Draktolli@users.noreply.github.com>
2020-12-31 16:17:59 +0300

    Update task4.bash

--- lab3/task4.bash
+++ lab3/task4.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-./task4_loop.sh &
+./task4_loop.bash &
 pid1=$!
-./task4_loop.sh &
+./task4_loop.bash &
 pid2=$!
-./task4_loop.sh &
+./task4_loop.bash &
 pid3=$!
 
 renice +10 -p $pid1
 kill $pid3
 
-at now + 1 minute <<< "pkill task4_while.bash"
+at now + 1 minute <<< "pkill task4_loop.bash"
